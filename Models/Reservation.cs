01645d21dfaf99bab8dc07b7eece3bab0880907d
Nour Zakarya <noorzakria25@gmail.com>
2024-05-04 07:33:56 +0300

    add new model(reservations)

---
+++ Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using Tazaker.Models;
+
+namespace Tazkara.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public DateTime ReserveDateTime { get; set; }
+        public int TicketId { get; set; }
+        public Ticket ticket { get; set; }
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser user { get; set; }
+    }
+}
